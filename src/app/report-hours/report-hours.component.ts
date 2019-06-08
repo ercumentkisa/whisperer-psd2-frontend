98e3b344a6c8d2873e7311c2d93852eb2a8442e9
Pavel Schoffer <xschof00@gmail.com>
2019-06-08 21:02:28 +0200

    Now do it

---
+++ src/app/report-hours/report-hours.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-report-hours',
+  templateUrl: './report-hours.component.html',
+  styleUrls: ['./report-hours.component.css'],
+})
+export class ReportHoursComponent implements OnInit {
+
+
+  data: any;
+
+  constructor() {
+    this.data = {
+      labels: ['0-4', '4-8', '8-12', '12-16', '16-18', '18-22', '22-24'],
+      datasets: [
+        {
+          label: 'Male',
+          data: [0, 3, 23, 43, 35, 59, 40],
+          fill: false,
+          borderColor: '#4bc0c0'
+        },
+        {
+          label: 'Female',
+          data: [1, 5, 64, 57, 93, 76, 12],
+          fill: false,
+          borderColor: '#f44242'
+        }
+      ]
+    }
+  }
+
+
+  ngOnInit() {
+  }
+
+}
